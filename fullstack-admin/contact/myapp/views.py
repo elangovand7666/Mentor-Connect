648158b40c9c25d01e4dee5e22f3490491cd78f8
D Elangovan <151810312+elangovand7666@users.noreply.github.com>
2025-01-06 11:47:27 +0530

    views.py

--- fullstack-admin/contact/myapp/views.py
+++ fullstack-admin/contact/myapp/views.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import gridfs
 
-client=MongoClient('mongodb+srv://es:es7666@cluster0.jjxan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client=MongoClient('mongodb+srv://mentorconnect2364:es7666@cluster0.byxk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 database=client['sample_db']
 collection=database['contacts']
 fs = gridfs.GridFS(database)
